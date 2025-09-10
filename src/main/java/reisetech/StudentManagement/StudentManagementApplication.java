3b37f4b4c5992992bed3f4718f1896112dfcb102
Ken Toku <kurusu83@gmail.com>
2025-09-10 23:31:16 +0900

    [23_MyBatisを使ってCRUDのRead処理を実装]の課題

--- src/main/java/reisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/reisetech/StudentManagement/StudentManagementApplication.java
@@ -1,20 +1,37 @@
 package reisetech.StudentManagement;
 
+import java.util.List;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+		@Autowired
+		private StudentRepository studentRepository;
+		@Autowired
+	  private StudentsCoursesRepository studentsCoursesRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
-	@GetMapping("/hello")
-	public String hello(){
-		return "Hello,World";
+
+	@GetMapping("/studentList")
+	public List<Student> getstudentList() {
+		return studentRepository.search();
+	}
+
+	@GetMapping("/studentsCourses")
+	public List<StudentsCourses> getStudentCoursesList(){
+		return studentsCoursesRepository.search();
 	}
 }
